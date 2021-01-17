e168545a93492cc6e01e92d0a7a535d4d70a7445
gojiteji <mail@gojiteji.com>
2021-01-17 17:18:25 +0900

    fix

---
+++ UnityAR/Assets/Scripts/Title.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Title : MonoBehaviour
+{
+    public void OnYomu()
+{
+    SceneManager.LoadScene("Scenes/yomu");
+}
+    public void OnMiru()
+{
+    SceneManager.LoadScene("Scenes/miru");
+}
+
+}
