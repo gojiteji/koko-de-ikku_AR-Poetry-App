7cfa9a893d86f75de08ce70a3901dc5ba27c188e
gojiteji <mail@gojiteji.com>
2021-02-15 10:52:47 +0900

    added scripts

---
+++ UnityAR/Assets/Scripts/MyBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+ 
+public class MyBehaviour : MonoBehaviour {
+    Texture myTexture;
+    void Start() {
+        StartCoroutine(GetTexture());
+    }
+ 
+    IEnumerator GetTexture() {
+        UnityWebRequest www = UnityWebRequestTexture.GetTexture("http://optipng.sourceforge.net/pngtech/img/lena.png");
+        yield return www.SendWebRequest();
+
+        if(www.isNetworkError || www.isHttpError) {
+            Debug.Log(www.error);
+        }
+        else {
+            GetComponent<Renderer>().material.mainTexture = ((DownloadHandlerTexture)www.downloadHandler).texture;
+        
+        }
+    }
+}
